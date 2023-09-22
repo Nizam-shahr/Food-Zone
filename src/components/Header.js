b50823456a1d5ec451a6065a0249cf266b469b22
Nizam-shahr <95461777+Nizam-shahr@users.noreply.github.com>
2023-09-22 11:32:27 +0100

    Update Header.js

--- src/components/Header.js
+++ src/components/Header.js
@@ -1,53 +1,106 @@
-'use client'
-import React, { use } from 'react'
-import { useSelector } from 'react-redux'
-import{signIn, signOut, useSession} from 'next-auth/react'
+import React, { useEffect, useState } from "react";
+import { useSelector } from "react-redux";
+import { getAuth, onAuthStateChanged } from "firebase/auth";
 import { AiOutlineShoppingCart } from "react-icons/ai";
-import { useRouter } from 'next/navigation'
-function Main() {
-  const router = useRouter()
-  const {items}  = useSelector((state) => state.cart);
-  const sumCount = items.reduce((initialSum, item) => initialSum + item.count, 0)
-  const {data: session} = useSession()
-const clickCart = (() => {
-   if (session) {
-   router.push('/Cart')
-  } else {
-    signIn()
+import { useRouter } from "next/navigation";
+import Modal from "react-modal";
+import Register from "./Register";
+import SignIn from "./SignIn";
+function Header() {
+  const router = useRouter();
+  const { items } = useSelector((state) => state.cart);
+  const sumCount = items.reduce(
+    (initialSum, item) => initialSum + item.count,
+    0
+  );
+  const auth  = getAuth()
+  const [isModalOpen, setIsModalOpen] = useState(false);
+  const [isOpen, setIsOpen]= useState(false)
+  const [loggedIn, setLoggedIn] = useState(false)
+  const modalStyle = {
+    overlay: {
+      backgroundColor: "rgba(0, 0, 0, 0.6)",
+      zIndex:'100',
+    },
+    content: {
+      top: "50%",
+      left: "50%",
+      right: "auto",
+      bottom: "auto",
+      marginRight: "-50%",
+      transform: "translate(-50%, -50%)",
+    },
+  };
+  const handleSignUp  = ()=> {
+    setIsOpen(true)
+  } 
+  const onLogOut = ()=> {
+    auth.signOut()
+    if (auth.signOut) {
+      alert('logged out')
+    } 
+    setLoggedIn(false)
   }
-})
- 
-  return (
-    <div className='container'>
-      <div className='header'>
-        <div onClick={() => router.push('/')} className='headerName'>
-          <h1>
-            Food<span className='zone'>Zone</span>
-          </h1>
-         
-        </div>
-        <div className='navBar'>
-          <div >
-          <div className='cart' onClick={clickCart}>
-            <h2>Cart</h2>
-             < AiOutlineShoppingCart className='shoppingCart'/>
-            {items.length !==0 ? <span className='cartLength'>{sumCount}</span>
-            : ''
+  const clickCart = () => {
+    if (loggedIn) {
+      router.push('/Cart')
+    }
+  
+  }
+  useEffect(() => {
+        onAuthStateChanged(auth, (data) => {
+            if(data){
+                setLoggedIn(true)
             }
-            
+            console.log(data);
+        })
+  }, [])
+
+  return (
+    <div className="container">
+      <div className="header">
+        <div onClick={() => router.push("/")} className="headerName">
+          <h1>
+            Food<span className="zone">Zone</span>
+          </h1>
+        </div>
+        <div className="navBar">
+          <div>
+            <div className="cart"onClick={clickCart} >
+              <h2>Cart</h2>
+              <AiOutlineShoppingCart className="shoppingCart" />
+              {items.length !== 0 ? (
+                <span className="cartLength">{sumCount}</span>
+              ) : (
+                ""
+              )}
+            </div>
           </div>
-              </div>
-            <div className='SignIn'  onClick={!session ? signIn : signOut}>           
-            <a className={session ? 'logIn' : 'loggedIn'} href=''> {session ? `Hello ${session.user.name}` : 'Sign In'}</a>
-            </div>
-            <div  onClick={!session? signIn : signOut}> 
-            <a className={!session ? 'signUp': 'signedUp'} href=''> {session ? `Hello ${session.user.name}` : 'Sign Up'}</a>
-            </div>
+          <div>  {loggedIn ?  <button onClick={onLogOut}>logout</button> : 
+           <button className="SignIn" onClick={() => setIsModalOpen(true)}>
+              Sign in
+            </button> }
+                 
+            <Modal isOpen={isModalOpen} onRequestClose={() => setIsModalOpen(false)} style={modalStyle} >
+                  <SignIn setIsModalOpen={setIsModalOpen} isModalOpen={isModalOpen} loggedIn={loggedIn}/>
+                  <button className="SignIn" onClick={handleSignUp} >
+              Sign Up Instead
+            </button> 
+                 
+            <Modal isOpen={isOpen} onRequestClose={() => setIsOpen(false)} style={modalStyle} >
+                  <Register setIsOpen={setIsOpen} setIsModalOpen={setIsModalOpen} loggedIn={loggedIn}/>
+                  
+                  <button  className="closeModal" onClick={()=> setIsOpen(false)} ><span>X</span></button>
+               
+            </Modal>
+                  <button  className="closeModal" onClick={()=> setIsModalOpen(false)} ><span>X</span></button>
+            </Modal>
+          
+          </div>
+        
         </div>
       </div>
-     
     </div>
-  )
-
-  }
-  export default Main
+  );
+}
+export default Header;
