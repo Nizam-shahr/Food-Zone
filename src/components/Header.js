01f2cd0ac7474c3c2c80facf73bdf3c52d233dc3
Nizam-shahr <95461777+Nizam-shahr@users.noreply.github.com>
2023-09-23 10:26:42 +0100

    Update Header.js

--- src/components/Header.js
+++ src/components/Header.js
@@ -104,11 +104,7 @@ function Header() {
             </button> }
                  
             <Modal isOpen={isModalOpen} onRequestClose={() => setIsModalOpen(false)} style={modalStyle} >
-                  <SignIn setIsModalOpen={setIsModalOpen} isModalOpen={isModalOpen} loggedIn={loggedIn} handleSignUp= {handleSignUp}/>
-                  <div className="create"> Don't have an account ?<button  onClick={handleSignUp} >
-                 Create One
-            </button></div>
-                   
+                  <SignIn setIsModalOpen={setIsModalOpen} isModalOpen={isModalOpen} loggedIn={loggedIn} handleSignUp= {handleSignUp}/>   
                  
             <Modal isOpen={isOpen} onRequestClose={() => setIsOpen(false)} style={secondModalStyle} >
                   <Register setIsOpen={setIsOpen} setIsModalOpen={setIsModalOpen} loggedIn={loggedIn}/>
