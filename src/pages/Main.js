60d409d65aa1cb43274a521e5f5fa7f8287e82d3
Nizam-shahr <95461777+Nizam-shahr@users.noreply.github.com>
2023-09-26 19:38:16 +0100

    Update Main.js

--- src/pages/Main.js
+++ src/pages/Main.js
@@ -54,7 +54,7 @@ console.log(selected);
              </select>
          </form>
        </div>
-       <section >
+       <section className='w-full'>
        <h2 className='interestTitle'> Pick an interest</h2>
        <div className='interest'>
         
@@ -63,19 +63,19 @@ console.log(selected);
          <h2 className= 'Nofilter' >Fast Food</h2>
          <p className='NofilterP'   >All sorrows are less with bread.</p>
         </button>
-        <button onClick={() => router.push('/Vendors')} disabled = {selected === '0' ? isDiabled : !isDiabled} className= {selected =='0' ? 'card filter grayscale' :'card filter grayscale-0' } >
+        <button onClick={() => router.push('/Vendors')} disabled = {selected === '0' ? isDiabled : !isDiabled} className= {selected =='0' ? 'card filter grayscale' :'card filter grayscale-0 cardHover' } >
         <img  src='./images/forkC.svg'  className='interestImage' alt='' />
         <h2 className='Nofilter' >Resturant</h2>
         <p className= 'NofilterP'>May not want it, but good for you.</p>
         </button>
        
-        <button onClick={() => router.push('/Vendors')} disabled = {selected === '0' ? isDiabled : !isDiabled} className= {selected =='0' ? 'card filter grayscale' :'card filter grayscale-0' } >
+        <button onClick={() => router.push('/Vendors')} disabled = {selected === '0' ? isDiabled : !isDiabled} className= {selected =='0' ? 'card filter grayscale ' :'card filter grayscale-0 cardHover' } >
         <img  src='./images/mai-thaiC.svg'   className='interestImage'  alt='' />
         <h2 className= 'Nofilter'>Drink And Cocktail </h2>
         <p className= 'NofilterP'>I feel sad for those who don't drink.</p>
         
         </button>
-      <button onClick={() => router.push('/Vendors')} disabled = {selected === '0' ? isDiabled : !isDiabled} className= {selected =='0' ? 'card filter grayscale' :'card filter grayscale-0' } >
+      <button onClick={() => router.push('/Vendors')} disabled = {selected === '0' ? isDiabled : !isDiabled} className= {selected =='0' ? 'card filter grayscale' :'card filter grayscale-0 cardHover' } >
         <img  src='./images/healthy-foodC.svg'  className='interestImage' alt='' />
         <h2  className= 'Nofilter'>Healthy Food</h2>
         <p className= 'NofilterP'>All sorrows are less with bread.</p>
@@ -89,20 +89,22 @@ console.log(selected);
        <section>
          <h2 className='headerTitle text-1.5'>How we serve you</h2>
          <div className="serveContainer">
+          <div className='serveContainerDiv'>
          <div className="serve">
            <Image src= './images/Fastfood_two.svg' width={250} height={100} />
-             <h2>Tell us where you are</h2>
+             <h2 className='text-center'>Tell us where you are</h2>
              <p className='filterP'>Select the location you want us to deliver.</p>
          </div>
          <div className="serve">
          <Image src= './images/Map_Two.svg'width={250} height={100}  />
-       <h2>Tell us what you want</h2>
+       <h2 className='text-center'>Tell us what you want</h2>
  <p className='filterP'>Browse the type of food that interest you.</p>
          </div>
          <div className="serve">
          <Image src= './images/Scooter_Two.svg' width={250} height={100} />
-         <h2>We’ll come running.</h2>
+         <h2 className='text-center'>We’ll come running.</h2>
          <p className='filterP'>Your order will be delivered to you in no time.</p>
+         </div>
          </div>
          </div>
        </section>
@@ -113,7 +115,7 @@ console.log(selected);
              <p className='footerTitle'>Contact Us</p>
              <div className='social'>
                <Image src='./images/twitter.svg'width="20" height="20" viewBox="0 0 20 20"   className='socailImage'/>
-               <Image  src='./images/facebook.svg' width="20" height="20" viewBox="0 0 20 20" className='socailImage' />
+               <Image  src='./images/facebook.svg' width="15" height="20" viewBox="0 0 20 20" className='socailImage' />
                <Image  src='./images/youtube.svg' width="20" height="20" viewBox="0 0 20 20" className='socailImage' />
              </div>
              
